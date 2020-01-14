93b476a043b109ee5a672b2c32a9b3427b201ce7
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-01-14 23:24:17 +0530

    comments  04

--- seosons_app/src/SeasonDisplay.js
+++ seosons_app/src/SeasonDisplay.js
@@ -11,9 +11,16 @@ const getSeason =(lat,month) =>{
 
 const SeasonDisplay=(props)=>{
    const season=getSeason(props.lat,new Date().getMonth());
+   const text=season==='winter' ? 'Burr, it is chilly' : 'Lets hit the beach'
 
-   console.log(season);
-    return <div> Season Display </div>
+//    if(season=='winter') 'its chilly'
+
+ 
+    return (
+    <div>
+        <h1>{text}</h1> 
+    </div>
+    );
 };
 
 export default SeasonDisplay;
