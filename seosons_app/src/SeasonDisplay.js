beaae25d0f80b0de755e46af39e93532056c018a
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-08-13 01:31:25 +0530

    comment

--- seosons_app/src/SeasonDisplay.js
+++ seosons_app/src/SeasonDisplay.js
@@ -1,26 +1,9 @@
 import React from 'react';
 
-const getSeason =(lat,month) =>{
-    if (month>2 && month>9)
-    {
-        return lat>0 ? 'summer': 'winter';
-    }else{
-        return lat >0 ? 'winter' : 'summer';
-    }
-}
+const SeasonDisplay=()=>{
 
-const SeasonDisplay=(props)=>{
-   const season=getSeason(props.lat,new Date().getMonth());
-   const text=season==='winter' ? 'Burr, it is chilly' : 'Lets hit the beach'
+        return <div>Seoson Display</div>
 
-//    if(season=='winter') 'its chilly'
-
- 
-    return (
-    <div>
-        <h1>{text}</h1> 
-    </div>
-    );
 };
 
 export default SeasonDisplay;
--- seosons_app/src/index.js
+++ seosons_app/src/index.js
@@ -4,45 +4,25 @@ import SeasonDisplay from './SeasonDisplay';
 
 
 
-class App extends React.Component{
-
-    constructor(props){
-        super(props);
-        this.state={lat:null,errorMessage: ''};
-
-      
-    }
-
-   state={lat:null, errorMessage: ''};
-    // componentDidMount(){
-    //         console.log('My component was rendered to the screen ');
-    // }
-
-    // componentDidUpdate(){
-    //     console.log('My component was just update-it rerender!');
-    // }
-
-    componentDidMount(){
+const App=()=>{
+    
         window.navigator.geolocation.getCurrentPosition(
-            position=> this.setState({lat: position.coords.latitude}),
-            err=> this.setState({errorMessage:err.message})
+            position=>console.log(position),
+            err=>console.log(err)
+        );
+
+    return <div>hi there</div>;
             
-             );
-    }
+            
 
-    render(){
-        if(this.state.errorMesage && !this.state.lat){
-            return <div>Error: {this.state.errorMessage}</div>
-        }
+};
 
-        if(!this.state.errorMessage && this.state.lat){
-            return <SeasonDisplay lat={this.state.lat}/>
-        }
-        return <div>Loading! </div>
-        
-    }
-      
-}
+
+ 
+
+
+
+
 
 
 ReactDOM.render(
