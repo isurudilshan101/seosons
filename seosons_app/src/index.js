9074593f7c7086468612cb16b81c69fedb3417d3
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2020-08-18 22:49:15 +0530

    comment

--- seosons_app/src/index.js
+++ seosons_app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 
 
@@ -35,7 +36,8 @@ import SeasonDisplay from './SeasonDisplay';
                  return <div>Error:{this.state.errorMessage}</div>
                  }
 
-                 return <div>Loading </div>
+
+                 return <Spinner />;
 
               
 
